9d31e40cf5a68be0c940e55eb8ee251435f98ac1
mosesbenjamin <mavewreek@gmail.com>
2021-06-03 23:28:13 +0100

    basics

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>About Jokes</h1>
+    <p>This is an app that displays bad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About The App',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'best place for bad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   <h2>Welcome to the best bad jokes app on the web</h2>
+ </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'best place for bad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
